0e88484d6ada76fe92a4e3781c693bf4995ca31b
Minh Nguyen <mnguyen368@gatech.edu>
2022-05-24 17:21:44 -0700

    Update Fashion_mnist.py

--- Python-ML-Fashion mnist/Fashion_mnist.py
+++ Python-ML-Fashion mnist/Fashion_mnist.py
@@ -26,6 +26,9 @@ import matplotlib.pyplot as plt
 
 data = keras.datasets.fashion_mnist
 
+##Fashion-MNIST is a dataset of Zalando's article images—consisting of a training set of 60,000 examples and 
+##a test set of 10,000 examples. Each example is a 28x28 grayscale image, associated with a label from 10 classes
+
 
 # In[30]:
 
